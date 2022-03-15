6c4b1ed9430ebf2ecd6e4dadb718bfc1dc299a33
Aydan Abrahams <aydan.b.abrahams@gmail.com>
2022-03-15 21:17:46 +0000

    Work Now Pls

--- src/handlers/messageHandler.ts
+++ src/handlers/messageHandler.ts
@@ -77,7 +77,7 @@ async function autoCreateThread(message: Message, requestId: Snowflake) {
 	});
 
 	const creationDate = message.createdAt.toISOString().slice(0, 10);
-	const messagetitle = message.content.slice(0, 97);
+	const messagetitle = message.content.slice(0, 97).trim();
 	console.log (messagetitle);
 	const authorName = authorMember === null || authorMember.nickname === null
 		? authorUser.username
