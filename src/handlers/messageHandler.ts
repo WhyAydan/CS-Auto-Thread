6d24346125dc18bc6c5ffd2d74002a01842dae8f
Aydan <76793618+WhyAydan@users.noreply.github.com>
2022-05-05 13:08:31 +0100

    Update messageHandler.ts

--- src/handlers/messageHandler.ts
+++ src/handlers/messageHandler.ts
@@ -48,7 +48,7 @@ async function autoCreateThread(message: Message, requestId: Snowflake) {
 	const authorMember = message.member;
 	const guild = message.guild;
 	const channel = message.channel;
-	const channelname = message.guild.channels.cache.find(channel => channel.name === "general");
+	const channelname = message.guild.channels.cache.channel.name;
 
 	let server = message.guild.id, // ID of the guild the message was sent in
 	chanid = message.channel.id // ID of the channel the message was sent in
